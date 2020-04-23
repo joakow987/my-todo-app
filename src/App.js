25f38222a6cf44ecabae47e9dfd3953e17ee352c
jkowalczyk <jkowalczyk@microstrategy.com>
2020-04-23 08:09:57 +0200

    Add finish button

--- src/App.js
+++ src/App.js
@@ -14,20 +14,20 @@ class App extends React.Component {
             todos: []
         }
     }
-    addTodoList = () => {
+    startTodoList = () => {
         this.setState(prevState => ({
             displayTodoList: true,
             inputValue: "",
             lists: [...prevState.lists, {name: this.state.inputValue, list: []}]
         }))
     }
-    handleChange = event => {
+    handleInputChange = event => {
         this.setState({
             inputValue: event.target.value
         })
     }
 
-    handleSubmit = (event) => {
+    addTodo = (event) => {
         event.preventDefault();
         if (this.state.inputValue !== "") {
             this.setState(prevState => ({
@@ -42,24 +42,29 @@ class App extends React.Component {
             todos: this.state.todos.map(todo => value === todo.value ? {value, checked: !checked} : todo)
         })
     }
+
+    addTodoList = () => {
+
+    }
     render() {
         return (
             <Grid id="app" container justify="center">
                 {this.state.displayTodoList
                     ?
                     <div>
-                        <form onSubmit={this.handleSubmit}>
+                        <form onSubmit={this.addTodoList}>
                             <div className="inputBox">
-                                <TextField id="outlined-basic" variant="outlined" onChange={this.handleChange} value={this.state.inputValue} />
+                                <TextField id="outlined-basic" variant="outlined" onChange={this.handleInputChange} value={this.state.inputValue} />
                             </div>
-                            <Button id="submitButton" variant="contained" color="primary" type="submit">Add a todo</Button>
+                            <Button id="submitButton" variant="contained" color="primary" onClick={this.addTodo}>Add a todo</Button>
+                            <Button id="finishButton" variant="contained" color="primary" type="submit">Finish list</Button>
                         </form>
                         <Todos todos={this.state.todos} onClick={this.handleCheckboxClick} />
                     </div>
                     :
-                    < form onSubmit={this.addTodoList}>
+                    < form onSubmit={this.startTodoList}>
                         <div id="inputField">
-                            <TextField label="Add a list name" variant="outlined" onChange={this.handleChange} value={this.state.inputValue} />
+                            <TextField label="Add a list name" variant="outlined" onChange={this.handleInputChange} value={this.state.inputValue} />
                         </div>
                         <Button id="appButton" variant="contained" color="primary" type="submit">Add a list</Button>
                     </form>
